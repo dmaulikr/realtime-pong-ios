7012d14ff2ee96af757d9ede6b062f6bf2d21cc0
Ruud Visser <visser.rgm@gmail.com>
2014-10-28 18:17:57 +0200

    Changed project to Objc

---
+++ PongPong/PongPong/PongPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PongPong
+//
+//  Created by Ruud Visser on 28-10-14.
+//  Copyright (c) 2014 Scrambled Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PongPong/PongPong/PongPong/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  PongPong
+//
+//  Created by Ruud Visser on 28-10-14.
+//  Copyright (c) 2014 Scrambled Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
